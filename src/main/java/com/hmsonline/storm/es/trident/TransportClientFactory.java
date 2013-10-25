9b67d40efa25794fe822a3ef9f520a69efaa4751
rmoquin <rmoquin@healthmarketscience.com>
2013-10-25 16:37:54 -0400

    Cache the elasticsearch client so that it is reused rather than a new one created each time it's requested.

--- src/main/java/com/hmsonline/storm/es/trident/TransportClientFactory.java
+++ src/main/java/com/hmsonline/storm/es/trident/TransportClientFactory.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,37 +14,39 @@ import java.util.Map;
 public class TransportClientFactory implements ClientFactory {
     private static final Logger LOG = LoggerFactory.getLogger(TransportClientFactory.class);
     public static final int DEFAULT_PORT = 9300;
-
+    private Client transportClient;
 
     @Override
     public Client makeClient(Map conf) {
-        ImmutableSettings.Builder settingsBuilder = ImmutableSettings.settingsBuilder();
+        if(transportClient == null) {
+            ImmutableSettings.Builder settingsBuilder = ImmutableSettings.settingsBuilder();
 
-        settingsBuilder.put("cluster.name", (String)conf.get(CLUSTER_NAME));
+            settingsBuilder.put("cluster.name", (String)conf.get(CLUSTER_NAME));
 
-        TransportClient client = new TransportClient(settingsBuilder);
+            TransportClient client = new TransportClient(settingsBuilder);
+            String clusterHosts = (String)conf.get(CLUSTER_HOSTS);
 
-        String clusterHosts = (String)conf.get(CLUSTER_HOSTS);
+            LOG.info("Creating TransportClient with addresses: '{}'", clusterHosts);
 
-        LOG.info("Creating TransportClient with addresses: '{}'", clusterHosts);
+            // expecting "host:port,host2:port2,host3"
+            if(!StringUtils.isEmpty(clusterHosts)){
+                String[] hostPorts = StringUtils.split(clusterHosts, ",");
+                for (String hostPortStr : hostPorts){
+                    String[] hostPort = StringUtils.split(hostPortStr, ":");
+                    if(hostPort.length == 2){
+                        client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], Integer.parseInt(hostPort[1])));
+                    } else if (hostPort.length == 1){
+                        client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], DEFAULT_PORT));
+                    }
 
-        // expecting "host:port,host2:port2,host3"
-        if(!StringUtils.isEmpty(clusterHosts)){
-            String[] hostPorts = StringUtils.split(clusterHosts, ",");
-            for (String hostPortStr : hostPorts){
-                String[] hostPort = StringUtils.split(hostPortStr, ":");
-                if(hostPort.length == 2){
-                    client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], Integer.parseInt(hostPort[1])));
-                } else if (hostPort.length == 1){
-                    client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], DEFAULT_PORT));
                 }
 
+            } else {
+                throw new IllegalStateException("Settings for '" + CLUSTER_HOSTS + "' can not be empty when using the Transport Client");
             }
-
-        } else {
-            throw new IllegalStateException("Settings for '" + CLUSTER_HOSTS + "' can not be empty when using the Transport Client");
+            transportClient = client;
         }
-
-        return client;
+        return transportClient;
     }
+
 }
